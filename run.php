814624069800144c722cddeb26c7632447a4d340
Caspar Green <cg@caspar.green>
2019-12-26 19:12:37 -0500

    Refactor run() to use SwiftMailer

--- run.php
+++ run.php
@@ -9,13 +9,21 @@
 namespace Webforms;
 
 use Dotenv\Dotenv;
+use Swift_Mailer;
+use Swift_SmtpTransport;
 
 function run(): string
 {
     $dotenv = Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 
-    $webform = new WebformProcessor();
+    $transport = (new Swift_SmtpTransport(getenv('SERVER'), getenv('PORT')))
+        ->setUsername(getenv('USER_ID'))
+        ->setPassword(getenv('PASSWD'));
+
+    $mailer = new Swift_Mailer($transport);
+
+    $webform = new WebformProcessor($mailer);
 
     return $webform->process();
 }
